66785ef0d51454ddfa7af67de7124f33f043446d
jinri <jinri@dfluid.kr>
2022-08-05 13:42:01 +0900

    fix. 카카오 인앱 100vh 스크롤문제 수정

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,33 @@ export default {
     Header,
     Main,
     Footer
+  },
+  methods: {
+    setScreenSize() {
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    },
+    disableScroll () {
+      document.querySelector('body').addEventListener('touchmove', this.removeEvent, { passive: false });
+      document.querySelector('body').addEventListener('onclick', this.removeEvent, { passive: false });
+      document.querySelector('body').addEventListener('mousewheel', this.removeEvent, { passive: false });
+    },
+    removeEvent (e) {
+      e.preventDefault();
+      e.stopPropagation();
+    },
+    enableScroll () {
+      document.querySelector('body').removeEventListener('touchmove', this.removeEvent);
+      document.querySelector('body').removeEventListener('onclick', this.removeEvent);
+      document.querySelector('body').removeEventListener('mousewheel', this.removeEvent);
+    }
+  },
+
+  created() {
+    this.setScreenSize();
+    this.disableScroll();
+    this.removeEvent();
+    this.enableScroll();
   }
 }
 </script>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,25 +1,8 @@
 <template>
   <div class="main">
     <div class="main-intro" id="main-intro">
-<!--      <div class="main-intro__text">-->
-<!--        <h2 class="text-tit">Save the Date</h2>-->
-<!--        <p class="text-info">-->
-<!--          2022. 09. 24 SAT 12:00 PM <br/>-->
-<!--          송파 예한교회 3층 예배당 <br/>-->
-<!--          <br/>-->
-<!--        </p>-->
-<!--      </div>-->
-<!--      <a class="main-intro__link" href="#">자세히 보기</a>-->
-<!--      <div class="main-intro__linkGroup">-->
-<!--        <a class="main-intro__link" href="#main-info">오시는 길</a>-->
-<!--        <div></div>-->
-<!--        <a class="main-intro__link" href="#main-info">연락처</a>-->
-<!--        <div></div>-->
-<!--        <a class="main-intro__link" href="#main-info">참석여부</a>-->
-<!--      </div>-->
     </div>
     <div class="main-info" id="main-info">
-      info
     </div>
     <div class="main-contact">
     </div>
@@ -30,6 +13,7 @@
 // import { ref, reactive } from 'vue';
 export default {
   setup() {
+
     return {
 
     }
